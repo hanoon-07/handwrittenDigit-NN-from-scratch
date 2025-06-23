df75f6af330eb78c5c544431ac25627184ef4981
hanoon-07 <muhhamedhanoon@gmail.com>
2025-06-23 19:24:16 +0530

    phase1- 95% peak accuracy achieved

---
+++ main.py
@@ -0,0 +1,11 @@
+import mnist_loader
+from network import Network
+
+# Load data
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+# Initialize network with 784 input neurons, 30 hidden neurons, and 10 output neurons
+net = Network([784, 30, 10])
+
+# Train network
+net.SGD(training_data, epochs=30, mini_batch_size=10, eta=3.0, test_data=test_data)
