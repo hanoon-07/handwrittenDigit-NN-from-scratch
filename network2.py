68205725d4798e38cf282c51a8ad00630af07f95
hanoon-07 <muhhamedhanoon@gmail.com>
2025-07-06 00:30:04 +0530

    cost class added

--- network2.py
+++ network2.py
@@ -19,3 +19,21 @@ import sys
 
 # Third-party libraries
 import numpy as np
+
+#### Define the quadratic and cross-entropy cost functions
+
+class QuadraticCost(object):
+
+    @staticmethod
+    def fn(a, y):
+        """Return the cost associated with an output ``a`` and desired output
+        ``y``.
+
+        """
+        return 0.5*np.linalg.norm(a-y)**2
+
+    @staticmethod
+    def delta(z, a, y):
+        """Return the error delta from the output layer."""
+        return (a-y) * sigmoid_prime(z)
+    
